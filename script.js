fe1e409c8f5566e2f98c9fd0e3da03d3b59a4aa2
Renata Rauber <renatarauber77@gmail.com>
2025-11-27 23:14:55 -0300

    sw

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/pomodoro/sw.js')
+        .register('/sw.js')
         .then(() => console.log('Service Worker registrado!'))
         .catch((err) => console.error('Erro ao registrar SW:', err));
 }
